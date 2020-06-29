94ce4e87b2121dfb35ccdf362f0135350988b4d4
Oleksandr Radosh <radosanya@gmail.com>
2020-06-29 14:12:09 +0300

    Added new functionality

--- OtherExersise/BattleShip/js/battelship.js
+++ OtherExersise/BattleShip/js/battelship.js
@@ -53,14 +53,97 @@ let view = {
 	}
 
 };
+let model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipSunk: 0,
 
-let ships = [
-	{ locations: ["10", "20", "30"], hits: ["", "", ""] },
-	{ locations: ["32", "33", "34"], hits: ["", "", ""] },
-	{ locations: ["63", "64", "65"], hits: ["", "", "hit"] },
+	ships: [
+		{ locations: ["14", "24", "34"], hits: ["", "", ""] },
+		{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+		{ locations: ["40", "50", "60"], hits: ["", "", ""] },
+	],
+	fire: function (guess) {
+		for (let i = 0; i < this.numShips; i++) {
+			let ship = this.ships[i];
+			let index = ship.locations.indexOf(guess);
+			if (index >= 0) {
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!")
+				if (this.isSunk(ship)) {
+					view.displayMessage("You sank my battleship!")
+					this.shipSunk++;
+				}
+				return true
+			}
+		}
+		view.displayMiss(guess);
+		view.displayMessage("You missed!")
+		return false
+	},
+	isSunk: function (ship) {
+		for (let i = 0; i < this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false
+			}
+		}
+		return true
+	}
+};
+let controller = {
+	guesses: 0,
+	processGuess: function (guess) {
+		let location = parseGuess(guess)
+		if (location) {
+			this.guesses++;
+			let hit = model.fire(location);
+			if (hit && model.shipSunk === model.numShips) {
+				view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses")
+			}
+		}
+	}
+};
+function parseGuess(guess) {
+	let alphabet = ["A", "B", "C", "D", "E", "F", "G"]
+	if (guess === null || guess.length !== 2) {
+		alert("Oops, please enter a letter and a number on the board.")
+	} else {
+		let firstChar = guess.charAt(0);
+		let row = alphabet.indexOf(firstChar);
+		let column = guess.charAt(1)
+		if (isNaN(row) || isNaN(column)) {
+			alert("Oops, that isn't on the board.")
+		} else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+			alert("Oops, that's off the board!")
+		} else {
+			return row + column;
+		}
+	}
+	return null
+};
 
-];
+function handFireButton() {
+	let guessInput = document.getElementById("guessInput");
+	let guess = guessInput.value;
+	controller.processGuess(guess);
 
-
+	guessInput.value = "";
+};
+function handKeyPress(e) {
+	let fireButton = document.getElementById("fireButton");
+	if (e.keyCode === 13) {
+		fireButton.click();
+		return false
+	}
+};
+function init() {
+	let fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handFireButton
+	let guessInput = document.getElementById("guessInput");
+	guessInput.onkeypress = handKeyPress
+};
+window.onload = init
 
 /*===========BattleShip==========*/
