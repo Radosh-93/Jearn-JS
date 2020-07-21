1c9b08ae1d92dc1557c2c10f2d2b9942a7ec9c63
Oleksandr Radosh <radosanya@gmail.com>
2020-07-21 23:56:13 +0300

    filter for properties, add btn to cart

--- VueJS/VueJsBook/js.js
+++ VueJS/VueJsBook/js.js
@@ -9,8 +9,32 @@ let webstore = new Vue({
 			description: 'A 25 pound bag of <em>irresistible</em>' + ' organic goodness for your cat.',
 			price: 2000,
 			image: 'assets/images/product-fullsize.png'
+		},
+		cart: [],
+	},
+	filters: {
+		formatPrice(price) {
+			if (!parseInt(price)) return "";
+			if (price > 99999) {
+				let priceString = (price / 100).toFixed(2);
+				let priceArray = priceString.split("").reverse();
+				let index = 3;
+				while (priceArray.length > index + 3) {
+					priceArray.splice(index + 3, 0, "'");
+					index += 4;
+				}
+				return "$" + priceArray.reverse().join("");
+			} else {
+				return "$" + (price / 100).toFixed(2);
+			}
 		}
 	},
+	methods: {
+		addToCart() {
+			this.cart.push(this.product.id);
+		}
+	},
+
 	beforeCreate() {
 		if (APP_LOG_LIFECYCLE_EVENTS) console.log('beaforeCreate')
 	},
