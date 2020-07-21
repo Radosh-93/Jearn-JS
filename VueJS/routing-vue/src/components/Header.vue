110573c6eb081804965589c4b5da65e53702ed98
Oleksandr Radoshniuk <olexandr.radosniuk@netzexpert.de>
2020-07-21 10:00:40 +0300

    create new

---
+++ VueJS/routing-vue/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "Communication between components in Vue.js"
+    };
+  }
+};
+</script>
+<style lang="css" scoped>
+div {
+  text-align: center;
+}
+h2 {
+  color: green;
+}
+</style>
---
+++ VueJS/routing-vue/src/components/ProductList.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <ProductItem v-for="(productItem, i) in products" :key="i" :product="productItem" :index="i" />
+  </ul>
+</template>
+
+<script>
+import ProductItem from "./ProductItems";
+import ProductService from "../productService";
+
+export default {
+  data() {
+    return {
+      products: []
+    };
+  },
+  components: {
+    ProductItem
+  },
+  created() {
+    this.products = ProductService.products;
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 400px;
+  padding: 0;
+  margin: 0 25px;
+}
+</style>
