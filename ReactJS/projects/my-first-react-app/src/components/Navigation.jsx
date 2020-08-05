1e7d11330c3d5b9c28d7561f0d4b9b34aa278448
Oleksandr Radoshniuk <olexandr.radosniuk@netzexpert.de>
2020-08-05 18:16:19 +0300

    css-modules

--- ReactJS/projects/my-first-react-app/src/components/Navigation.jsx
+++ ReactJS/projects/my-first-react-app/src/components/Navigation.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import s from './Navigation.module.css'
 
 const Navigation = () => {
 	return (
-		<nav className="nav">
+		<nav className={s.nav}>
 			<ul>
-				<li><a href="#">Profile</a></li>
+				<li><a className={`${s.item} ${s.active}`} href="#">Profile</a></li>
 				<li><a href="#">Messages</a></li>
 				<li><a href="#">News</a></li>
 				<li><a href="#">Music</a></li>
--- ReactJS/projects/my-first-react-app/src/components/Profile.jsx
+++ ReactJS/projects/my-first-react-app/src/components/Profile.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = () => {
 	return (
-		<main className="content">
-			<div className="wallpaper">
+		<main className={s.content}>
+			<div className={s.wallpaper}>
 				<img src="https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png" />
 			</div>
-			<div className="profile-information">
+			<div className={s.profile_information}>
 				<img src="https://selfie2anime.com/img/carousel/2.jpg" />
-				<div className="about-profile">
+				<div className={s.about_profile}>
 					<p>Dmitry K.</p>
 					<span>Date of Birth: 2 January</span>
 					<span>City: Minsk</span>
@@ -16,22 +17,22 @@ const Profile = () => {
 					<span>Web Site: <a href="https://www.it-kamasutra.com">https://www.it-kamasutra.com</a></span>
 				</div>
 			</div>
-			<div className="posts">
+			<div className={s.posts}>
 				<h3>My posts</h3>
 				<form>
 					<textarea placeholder="your news..."></textarea>
 					<button>Send</button>
 				</form>
-				<div className="users-posts">
-					<img className="icon" src="" />
+				<div className={s.users_posts}>
+					<img className={s.icon} src="" />
 					<p>Hey, why nobody love me?</p>
 				</div>
-				<div className="users-posts">
-					<img className="icon" src="" />
+				<div className={s.users_posts}>
+					<img className={s.icon} src="" />
 					<p>It's our new programm! Hey!</p>
 				</div>
-				<div className="users-posts">
-					<img className="icon" src="" />
+				<div className={s.users_posts}>
+					<img className={s.icon} src="" />
 					<p>Sometext in the body</p>
 				</div>
 			</div>
