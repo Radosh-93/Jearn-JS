452e465e7ce9a7bce8d172c4789d91e4f8d83d74
Oleksandr Radoshniuk <olexandr.radosniuk@netzexpert.de>
2020-08-11 14:33:15 +0300

    OOP, refactoring

--- ReactJS/projects/my-first-react-app/src/index.js
+++ ReactJS/projects/my-first-react-app/src/index.js
@@ -1,8 +1,20 @@
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state'
-import { rerenderEntireTree } from './render';
 
-rerenderEntireTree(state);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './redux/state'
+
+export let rerenderEntireTree = (state) => {
+	ReactDOM.render(
+		<App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} sendMessage={store.sendMessage.bind(store)} updateNewMesageText={store.updateNewMesageText.bind(store)} />,
+		document.getElementById('root')
+	);
+};
+
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
