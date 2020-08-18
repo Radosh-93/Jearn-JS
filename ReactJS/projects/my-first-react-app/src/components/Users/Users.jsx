4ef3de67382d32822e39352c29644b61eeec1671
Oleksandr Radoshniuk <olexandr.radosniuk@netzexpert.de>
2020-08-18 11:59:21 +0300

    Class Component

--- ReactJS/projects/my-first-react-app/src/components/Users/Users.jsx
+++ ReactJS/projects/my-first-react-app/src/components/Users/Users.jsx
@@ -1,51 +1,45 @@
 import React from "react";
 import s from './Users.module.css'
 import { Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import * as axios from "axios";
 import userPhoto from '../../assets/images/user_img.png'
 
-const useStyles = makeStyles((theme) => ({
-	button: {
-		textTransform: 'none',
-		cursor: 'pointer'
-	},
-}));
-
-const Users = (props) => {
-	const classes = useStyles();
-	if (props.usersData.length === 0) {
+class Users extends React.Component{
+	constructor(props) {
+		super(props);
 		axios.get("https://social-network.samuraijs.com/api/1.0/users")
 			.then(response => {
-			props.setUsers(response.data.items)})
+				this.props.setUsers(response.data.items)})
 	}
-	return (
-		<div className={s.users_page}>
-			<h1 className={s.main_title}>Users</h1>
-			{props.usersData.map(el => (
-				<div key={el.id} className={s.user_block}>
-					<img className={s.photo} src={el.photos.small !== null ? el.photos.small : userPhoto} alt='photo' />
-					{el.followed ? <Button
-						onClick={() => { props.onUnfollow(el.id) }}
-						variant="contained"
-						color="secondary"
-						className={`${s.btn} ${classes.button}`}>
-						Unfollow
-					</Button> : <Button
-							onClick={() => { props.onFollow(el.id) }}
+	render() {
+		return (
+			<div className={s.users_page}>
+				<h1 className={s.main_title}>Users</h1>
+				{this.props.usersData.map(el => (
+					<div key={el.id} className={s.user_block}>
+						<img className={s.photo} src={el.photos.small !== null ? el.photos.small : userPhoto} alt='photo' />
+						{el.followed ? <Button
+							onClick={() => { this.props.onUnfollow(el.id) }}
+							variant="contained"
+							color="secondary"
+							className={`${s.btn}`}>
+							Unfollow
+						</Button> : <Button
+							onClick={() => { this.props.onFollow(el.id) }}
 							variant="contained"
 							color="primary"
-							className={`${s.btn} ${classes.button}`}>
+							className={`${s.btn}`}>
 							Follow
-					</Button>}
-					<div className={s.about_user}>
-						<span className={s.name}>{el.name}</span>
-						<span className={s.location}>{"${el.location.city}, ${el.location.country}"}</span>
-						<span className={s.status}>{el.status}</span>
-					</div>
-				</div>))}
-		</div>
-	)
+						</Button>}
+						<div className={s.about_user}>
+							<span className={s.name}>{el.name}</span>
+							<span className={s.location}>{"${el.location.city}, ${el.location.country}"}</span>
+							<span className={s.status}>{el.status}</span>
+						</div>
+					</div>))}
+			</div>
+		)
+	}
 }
 
 export default Users
